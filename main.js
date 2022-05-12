c5c747c1f8cf9fcc0957f7d96114b368e25970cf
KRUTIKHIRAPARA <krutik2511@gmail.com>
2022-05-12 05:14:02 -0700

    Calculator Project

---
+++ main.js
@@ -0,0 +1,31 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenvalue = '';
+
+for(item of buttons)
+{
+    item.addEventListener('click',(e) => {
+        buttonText = e.target.innerText;
+        if(buttonText == 'X')
+        {
+            buttonText= '*';
+            screenvalue += buttonText;
+            screen.value = screenvalue;
+        }
+        else if(buttonText == 'C')
+        {
+            screenvalue = "";
+            screen.value = screenvalue;
+        }
+        else if(buttonText == '=')
+        {
+            screen.value = eval(screenvalue);
+        }
+        else
+        {
+            screenvalue += buttonText;
+            screen.value = screenvalue;
+        }
+        
+    });
+}
